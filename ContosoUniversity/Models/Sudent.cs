02f401f6d305742c0035d1f82d691703d379a330
Pierre Colin Fujlap <pierre.colin@vdab.be>
2014-08-01 14:09:13 +0200

    entityframework package (mislikt)
    enrollment, student

---
+++ ContosoUniversity/Models/Sudent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.Models
+{
+    public class Sudent
+    {
+        public int ID { get; set; }
+        public String LastName { get; set; }
+        public String FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+
+
+
+}
